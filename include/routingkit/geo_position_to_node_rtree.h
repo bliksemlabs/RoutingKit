e7e245ad46730e91bea22aac15c20fcf40a20340
Stefan de Konink <stefan@konink.de>
2020-04-20 18:54:07 +0200

    Dump code for Nikhil

--- include/routingkit/geo_position_to_node_rtree.h
+++ include/routingkit/geo_position_to_node_rtree.h
@@ -26,8 +26,15 @@ public:
     GeoPositionToNodeRTree(const std::vector<float>&latitude, const std::vector<float>&longitude, const std::vector<unsigned>&first_out, const std::vector<unsigned>&head, const std::vector<unsigned>&tail, const std::vector<unsigned>&first_modelling_node, const std::vector<float>&modelling_node_latitude, const std::vector<float>&modelling_node_longitude);
 
     RTreeQueryResult find_nearest_neighbor(float query_latitude, float query_longitude); // TODO: should be const, RTree doesn't do it
+    std::vector<RTreeQueryResult> find_all(float query_latitude, float query_longitude); // TODO: should be const, RTree doesn't do it
 
 // private:
+    void expand_search_rect(float &query_latitude, float &query_longitude, float (&search_rect_min)[2], float (&search_rect_max)[2], float &radius, std::vector<unsigned> &results);
+    void shrink_optimal_rect(float &query_latitude, float &query_longitude, float (&optimal_rect_min)[2], float (&optimal_rect_max)[2], float &radius);
+    bool within_optimal_rect(float (&optimal_rect_min)[2], float (&optimal_rect_max)[2], std::vector<unsigned> &results, unsigned &min_diff_id);
+    void expand_contained_rect(float (&optimal_rect_min)[2], float (&optimal_rect_max)[2], unsigned &min_diff_id, float (&search_rect_min)[2], float (&search_rect_max)[2]);
+    bool validate_search_rect(float (&search_rect_min)[2], float (&search_rect_max)[2], std::vector<unsigned> &results);
+
     std::vector<unsigned> head;
     std::vector<unsigned> tail;
     std::vector<float> latitude;
--- src/geo_position_to_node_rtree.cpp
+++ src/geo_position_to_node_rtree.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 // TODO: This is not supposed to, any better w
 // MyTree tree;
@@ -16,6 +19,56 @@ namespace {
         vector.push_back(id);
         return true; // keep going
     }
+
+json geojson_feature_polygon_from_envelope(float (&rect_min)[2], float (&rect_max)[2], std::string id, std::string orig_id) {
+    return {
+        { "id", id },
+        { "properties",
+            {
+                { "orig_id", orig_id }
+            }
+        },
+        { "type", "Feature" },
+        { "geometry",
+            {
+                { "type", "Polygon" },
+                { "coordinates",
+                    {
+                        {
+                            { rect_min[0], rect_min[1] },
+                            { rect_min[0], rect_max[1] },
+                            { rect_max[0], rect_max[1] },
+                            { rect_max[0], rect_min[1] },
+                            { rect_min[0], rect_min[1] }
+                        }
+                    }
+                }
+            }
+        }
+    };
+}
+
+json geojson_feature_point(float longitude, float latitude, std::string id, std::string orig_id) {
+    return {
+        { "id", id },
+        { "properties",
+            {
+                { "orig_id", orig_id }
+            }
+        },
+        { "type", "Feature" },
+        { "geometry",
+            {
+                { "type", "Point" },
+                { "coordinates",
+                    { longitude, latitude }
+                }
+            }
+        }
+    };
+}
+
+
 }
 
 GeoPositionToNodeRTree::GeoPositionToNodeRTree(const std::vector<float>&latitude, const std::vector<float>&longitude, const std::vector<unsigned>&first_out, const std::vector<unsigned>&head, const std::vector<unsigned>&tail, const std::vector<unsigned>&first_modelling_node, const std::vector<float>&modelling_node_latitude, const std::vector<float>&modelling_node_longitude):
@@ -251,9 +304,9 @@ GeoPositionToNodeRTree::RTreeQueryResult GeoPositionToNodeRTree::find_nearest_ne
                 }
             }
 
+			traversed += geo_dist(prev_lat, prev_lon, modelling_node_latitude[m], modelling_node_longitude[m]);
 			prev_lat = modelling_node_latitude[m];
 			prev_lon = modelling_node_longitude[m];
-			traversed += geo_dist(prev_lat, prev_lon, modelling_node_latitude[m], modelling_node_longitude[m]);
 		}
 
 		geo_dist_projected_to_arc(prev_lat, prev_lon, latitude[y], longitude[y], query_latitude, query_longitude, a_dist, b_dist, pp_dist, pp_lat, pp_lon);
@@ -316,5 +369,295 @@ GeoPositionToNodeRTree::RTreeQueryResult GeoPositionToNodeRTree::find_nearest_ne
 
     return result;
 }
+
+void GeoPositionToNodeRTree::expand_search_rect(float &query_latitude, float &query_longitude, float (&search_rect_min)[2], float (&search_rect_max)[2], float &radius, std::vector<unsigned> &results) {
+    // std::cout << query_latitude << "," << query_longitude << std::endl;    
+    // std::cout << geojson_feature_point(query_longitude, query_latitude, "query", "query").dump(1) << ",";
+
+
+    int nhits = 0;
+	while (nhits == 0) {
+		radius *= 2;
+
+		search_rect_min[0] = query_longitude - radius;
+		search_rect_min[1] = query_latitude - radius;
+		search_rect_max[0] = query_longitude + radius;
+		search_rect_max[1] = query_latitude + radius;
+
+		nhits = tree.Search(search_rect_min, search_rect_max, MySearchCallback, (void *) &results);
+	    
+        // std::cout << "expand_search_rect " << radius << nhits << std::endl;
+	    // std::cout << "expand_search_rect " << search_rect_min[0] << ", " << search_rect_min[1] << " " << search_rect_max[0] << ", " << search_rect_max[1] << std::endl;
+
+        // std::cout << geojson_feature_polygon_from_envelope(search_rect_min, search_rect_max, "expand_search_rect", "expand_search_rect").dump(1) << "," << std::endl;
+    }
+}
+
+void GeoPositionToNodeRTree::shrink_optimal_rect(float &query_latitude, float &query_longitude, float (&optimal_rect_min)[2], float (&optimal_rect_max)[2], float &radius) {
+	// std::cout << query_latitude << "," << query_longitude << std::endl;
+
+	float optimal = radius / 1.41f;
+
+	optimal_rect_min[0] = query_longitude - optimal;
+	optimal_rect_min[1] = query_latitude - optimal;
+	optimal_rect_max[0] = query_longitude + optimal;
+	optimal_rect_max[1] = query_latitude + optimal;
+
+	// std::cout << "shrink_optimal_rect " << optimal_rect_min[0] << ", " << optimal_rect_min[1] << " " << optimal_rect_max[0] << ", " << optimal_rect_max[1] << std::endl;
+
+    // std::cout << geojson_feature_polygon_from_envelope(optimal_rect_min, optimal_rect_max, "expand_optimal_rect", "shrink_optimal_rect").dump(1) << "," << std::endl;
+}
+
+
+bool GeoPositionToNodeRTree::within_optimal_rect(float (&optimal_rect_min)[2], float (&optimal_rect_max)[2], std::vector<unsigned> &results, unsigned &min_diff_id) {
+	float min_diff = inf_weight;
+	for (unsigned i : results) {
+		if (optimal_rect_min[0] <= imin[i][0] && optimal_rect_min[1] <= imin[i][1] && optimal_rect_max[0] >= imax[i][0] && optimal_rect_max[1] >= imax[i][1]) {
+			min_diff_id = invalid_id;
+			return true;
+		} else {
+			float overlap_x = fmax(0, fmin(optimal_rect_max[0], imax[i][0]) - fmax(optimal_rect_min[0], imin[i][0]));
+			float overlap_y = fmax(0, fmin(optimal_rect_max[1], imax[i][1]) - fmax(optimal_rect_min[1], imin[i][1]));
+			float surface_o = overlap_x * overlap_y;
+			float surface_i = (imax[i][0] - imin[i][0]) * (imax[i][1] - imin[i][1]);
+			float diff = surface_i - surface_o;
+            
+            // std::cout << "within_optimal_rect " << imin[i][0] << ", " << imin[i][1] << " " << imax[i][0] << ", " << imax[i][1] << std::endl;
+
+			if (diff < min_diff) {
+				min_diff_id = i;
+				min_diff = diff;
+
+                float min_convert[2] = { imin[i][0], imin[i][1] };
+                float max_convert[2] = { imax[i][0], imax[i][1] };
+
+                // std::cout << geojson_feature_polygon_from_envelope(min_convert, max_convert, std::to_string(i), "within_optimal_rect").dump() << "," << std::endl;
+                // std::cout << "within_optimal_rect " << min_diff_id << " (" << min_diff << ")" << std::endl;
+			}
+		}
+	}
+
+	return false;
+}
+
+void GeoPositionToNodeRTree::expand_contained_rect(float (&optimal_rect_min)[2], float (&optimal_rect_max)[2], unsigned &min_diff_id, float (&search_rect_min)[2], float (&search_rect_max)[2]) {
+	// std::cout << "NOT CONTAINED" << std::endl;
+
+	float max_diff = 0.0f;
+	if (imax[min_diff_id][0] > optimal_rect_max[0]) {
+		float diff = imax[min_diff_id][0] - optimal_rect_max[0];
+		if (diff > max_diff) max_diff = diff;
+	}
+	if (imax[min_diff_id][1] > optimal_rect_max[1]) {
+		float diff = imax[min_diff_id][1] - optimal_rect_max[1];
+		if (diff > max_diff) max_diff = diff;
+	}
+	if (imin[min_diff_id][0] < optimal_rect_min[0]) {
+		float diff = optimal_rect_min[0] - imin[min_diff_id][0];
+		if (diff > max_diff) max_diff = diff;
+	}
+	if (imin[min_diff_id][1] < optimal_rect_min[1]) {
+		float diff = optimal_rect_min[1] - imin[min_diff_id][1];
+		if (diff > max_diff) max_diff = diff;
+	}
+	
+	// std::cout << "Max diff: " << max_diff << std::endl;
+
+	search_rect_min[0] = optimal_rect_min[0] - max_diff;
+	search_rect_min[1] = optimal_rect_min[1] - max_diff;
+	search_rect_max[0] = optimal_rect_max[0] + max_diff;
+	search_rect_max[1] = optimal_rect_max[1] + max_diff;
+
+	
+    // std::cout << geojson_feature_polygon_from_envelope(search_rect_min, search_rect_max, "expand_search_rect", "expand_contained_rect").dump(1) << "," << std::endl;
+	// std::cout << "expand_contained_rect " << search_rect_min[0] << ", " << search_rect_min[1] << " " << search_rect_max[0] << ", " << search_rect_max[1] << std::endl;
+}
+
+bool GeoPositionToNodeRTree::validate_search_rect(float (&search_rect_min)[2], float (&search_rect_max)[2], std::vector<unsigned> &results) {
+	int nhits = tree.Search(search_rect_min, search_rect_max, MySearchCallback, (void *) &results);
+	// std::cout << nhits << std::endl;
+	for (unsigned i : results) {
+		if (search_rect_min[0] <= imin[i][0] && search_rect_min[1] <= imin[i][1] && search_rect_max[0] >= imax[i][0] && search_rect_max[1] >= imax[i][1]) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+
+std::vector<GeoPositionToNodeRTree::RTreeQueryResult> GeoPositionToNodeRTree::find_all(float query_latitude, float query_longitude) {
+   
+    std::vector<GeoPositionToNodeRTree::RTreeQueryResult> ret;
+
+	/* we store the envelope for each iteration until we have a hit */ 
+	float search_rect_min[2];
+	float search_rect_max[2];
+
+	std::vector<unsigned> results;
+	results.reserve(20);
+
+    /* we expand the range around the latitude/longitude by this factor */ 
+    float radius = 0.00005f;
+
+    expand_search_rect(query_latitude, query_longitude, search_rect_min, search_rect_max, radius, results);
+
+	/* imagine that we have found a result given a certain radius from our search position.
+       the result is rectangle based, hence worst case it could be in the corners of the square
+       enveloping the circle enveloping the radius. [o]
+       The unit vector of the radius matching the inset [( )] will have a scaled radius of 1/sqrt(2) = 1 / (r / math.sqrt((r*r) + (r*r)))
+       the optimal rect envelopes that radius.
+       We can now guarantee that everything will be found, will be closest with respect of the rest of the set.
+     */
+
+	float optimal_rect_min[2];
+	float optimal_rect_max[2];
+	
+	shrink_optimal_rect(query_latitude, query_longitude, optimal_rect_min, optimal_rect_max, radius);
+		
+	/* A second guarantee that we need is the knowledge of full coverage.
+       If any envelope is fully contained within our optimal_rect we can be sure that any result in that set is closest.
+       If no envelope is fully contained we must expand our search region.
+       For optimisation purposes it might be more efficient to have bigger resultset, to guarantee that any of its results is contained. 
+     */
+
+	unsigned min_diff_id = invalid_id;
+	bool has_contained = within_optimal_rect(optimal_rect_min, optimal_rect_max, results, min_diff_id);
+
+	/* we need to search a bit broader to have contained results */
+	if (!has_contained) {
+		expand_contained_rect(optimal_rect_min, optimal_rect_max, min_diff_id, search_rect_min, search_rect_max);
+		has_contained = validate_search_rect(search_rect_min, search_rect_max, results);
+	}
+
+
+
+	/* This part of the code is the true quality improvement for georeferencing.
+       Given the results above we know for sure that there will exist a result that is closest to our search origin.
+       Given that the results only have an envelope we will now iterate over the first and last point, and the modelling nodes.
+       Each arc is evaluated on proximity to the search origin.
+       Due to the fact that a way may be traversed in both directions, two arcs may be considered optimum.
+	 */ 
+
+	for (unsigned xy : results) {
+        float best_traversed = INFINITY;
+        float best_distance = INFINITY;
+        float best_realdistance = INFINITY;
+        unsigned best_id = invalid_id;
+        unsigned best_m = invalid_id;
+        float best_pp_lat;
+        float best_pp_lon;
+
+		unsigned x = tail[xy];
+		unsigned y = head[xy];
+
+		float prev_lat = latitude[x];
+		float prev_lon = longitude[x];
+
+		float traversed = 0.0f;
+
+		double a_dist, b_dist, pp_dist, pp_lat, pp_lon;
+
+		for (unsigned m = first_modelling_node[xy]; m < first_modelling_node[xy + 1]; ++m) {
+			geo_dist_projected_to_arc(prev_lat, prev_lon, modelling_node_latitude[m], modelling_node_longitude[m], query_latitude, query_longitude, a_dist, b_dist, pp_dist, pp_lat, pp_lon);
+
+			// float distance = traversed + (pp_dist * pp_dist);
+			float distance = pp_dist;
+			if (distance < best_distance) {
+				best_realdistance = traversed + pp_dist;
+                best_traversed = traversed;
+				best_distance = distance;
+				best_id = xy;
+				best_pp_lat = pp_lat;
+				best_pp_lon = pp_lon;
+                best_m = m;
+			} else if (distance == best_distance) {
+                /* here we would prefer to snap to the edge in the direction the road, country specific */
+                
+                float delta_lat = modelling_node_latitude[m] - prev_lat;
+                float delta_lon = modelling_node_longitude[m] - prev_lon;
+                float delta_query_lat = query_latitude - prev_lat;
+                float delta_query_lon = query_longitude - prev_lon;
+
+                /* https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located */
+                float d = (delta_query_lon * delta_lat) - (delta_query_lat * delta_lon);
+                if (d > 0.0f) {
+                    best_id = xy;
+                    best_m = m;
+                } else if (d == 0.0f) {
+                    /* consider that we are on the line, lets snap to the nearest distance
+                    * TODO: test
+                    */
+                    float tmp_realdistance = traversed + pp_dist;
+                    if (tmp_realdistance < best_realdistance) {
+                        best_realdistance = tmp_realdistance;
+                        best_traversed = traversed;
+                        best_id = xy;
+                        best_m = m;
+                    }
+                }
+            }
+
+			traversed += geo_dist(prev_lat, prev_lon, modelling_node_latitude[m], modelling_node_longitude[m]);
+			prev_lat = modelling_node_latitude[m];
+			prev_lon = modelling_node_longitude[m];
+		}
+
+		geo_dist_projected_to_arc(prev_lat, prev_lon, latitude[y], longitude[y], query_latitude, query_longitude, a_dist, b_dist, pp_dist, pp_lat, pp_lon);
+		// float distance = traversed + (pp_dist * pp_dist);
+        float distance = pp_dist;
+        // std::cout << "pp_dist: " << pp_dist << std::endl;
+		if (distance < best_distance) {
+			best_realdistance = traversed + pp_dist;
+			best_distance = distance;
+            best_traversed = traversed;
+			best_id = xy;
+			best_pp_lat = pp_lat;
+			best_pp_lon = pp_lon;
+            best_m = first_modelling_node[xy + 1];
+		} else if (distance == best_distance) {
+            /* here we would prefer to snap to the edge in the direction the road, country specific */
+            
+            float delta_lat = latitude[y] - prev_lat;
+            float delta_lon = longitude[y] - prev_lon;
+            float delta_query_lat = query_latitude - prev_lat;
+            float delta_query_lon = query_longitude - prev_lon;
+
+            /* https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located */
+            float d = (delta_query_lon * delta_lat) - (delta_query_lat * delta_lon);
+            if (d > 0.0f) {
+                best_id = xy;
+                best_m = first_modelling_node[xy + 1];
+            } else if (d == 0.0f) {
+                /* consider that we are on the line, lets snap to the nearest distance
+                 * TODO: test
+                 */
+                float tmp_realdistance = traversed + pp_dist;
+                if (tmp_realdistance < best_realdistance) {
+                    best_realdistance = tmp_realdistance;
+                    best_id = xy;
+                    best_m = first_modelling_node[xy + 1];
+                }
+            }
+        }
+
+        RTreeQueryResult result = { invalid_id, invalid_id, INFINITY, INFINITY, INFINITY, INFINITY };
+        if (best_id != invalid_id) {
+            result.xy = best_id;
+            result.m = best_m;
+            result.distance_since_origin = best_traversed;
+            result.distance_to_edge = best_distance;
+            result.longitude = best_pp_lon;
+            result.latitude = best_pp_lat;
+            ret.push_back(result);
+        }
+	}
+
+    return ret;
+}
+
+
+
 
 } // RoutingKit
